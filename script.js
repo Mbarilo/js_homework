323d1595e9b4e42ad034a10ca7f58ffcf714e73d
Mbarilo <mishapeters7@gmail.com>
2025-09-22 22:33:47 +0300

    add post.json

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const moment = require("moment");
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const PORT = 8000;
@@ -13,6 +15,13 @@ app.get("/timestamp", (req, res) => {
   res.json({ timestamp: getDate()});
 });
 
+app.get("/posts", (req, res) => {
+  const PathToFile = path.join(__dirname, "post.json");
+  fs.readFile(PathToFile, "utf-8", (err, data) => {
+    res.json(JSON.parse(data));
+  });
+});
+
 app.listen(PORT, () => {
-  console.log(`Сервер запущен: http://localhost:${PORT}`);
+  console.log(`Сервер ввімкнений: http://localhost:${PORT}`);
 });
