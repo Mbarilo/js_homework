596de251b9a69112caeefbe0a43c4d5820b06eaf
Mbarilo <mishapeters7@gmail.com>
2025-09-26 20:44:49 +0300

    add take & give, and func to give post from id

--- script.js
+++ script.js
@@ -12,16 +12,39 @@ function getDate() {
 }
 
 app.get("/timestamp", (req, res) => {
-  res.json({ timestamp: getDate()});
+  res.json({ timestamp: getDate() });
 });
 
 app.get("/posts", (req, res) => {
-  const PathToFile = path.join(__dirname, "post.json");
-  fs.readFile(PathToFile, "utf-8", (err, data) => {
-    res.json(JSON.parse(data));
+  const filePath = path.join(__dirname, "post.json");
+  fs.readFile(filePath, "utf-8", (data) => {
+    const posts = JSON.parse(data);
+    let { skip, take } = req.query;
+
+    skip = skip ? parseInt(skip) : 0;
+    take = take ? parseInt(take) : posts.length;
+
+    const result = posts.slice(skip, skip + take);
+    res.json(result);
+  });
+});
+
+app.get("/posts/:id", (req, res) => {
+  const filePath = path.join(__dirname, "post.json");
+  fs.readFile(filePath, "utf-8", (err, data) => {
+
+    const posts = JSON.parse(data);
+    const id = parseInt(req.params.id);
+
+    const post = posts.find((p) => p.id === id);
+    if (!post) {
+      return res.status(404).json({ error: "Пост не знайдено" });
+    }
+
+    res.json(post);
   });
 });
 
 app.listen(PORT, () => {
-  console.log(`Сервер ввімкнений: http://localhost:${PORT}`);
+  console.log(`Сервер ввімкнено: http://localhost:${PORT}`);
 });
