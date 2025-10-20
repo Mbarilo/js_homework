0c50498caedb4d299944bbbcdc4c22b50065e9b8
Mbarilo <mishapeters7@gmail.com>
2025-10-20 22:39:38 +0300

    add prisma

---
+++ prisma/seed.ts
@@ -0,0 +1,45 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const tag1 = await prisma.tag.create({data:{ name: "Hello"}});
+  const tag2 = await prisma.tag.create({data:{ name: "Hi"}});
+
+  const post = await prisma.post.create({
+    data: {
+      title: "перший пост",
+      description: "гіикререичврм",
+      image: "https://123.com",
+      likes: 10,
+      tags: {
+        create: [
+          {tag:{connect:{id: tag1.id}}},
+          {tag:{connect:{id: tag2.id}}}
+        ]
+      }
+    },
+    include: {tags:{include:{tag: true}}}
+  });
+
+  const post2 = await prisma.post.create({
+    data: {
+      title: "другий пост",
+      description: "еруикгікеигім",
+      image: "https://123.com",
+      likes: 3,
+      tags: {
+        create: [
+          {tag:{connect:{id: tag1.id}}}
+        ]
+      }
+    },
+    include:{tags:{include:{tag: true}}}
+  });
+
+  console.log("икгкесмкег", post, post2);
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(() => prisma.$disconnect());
